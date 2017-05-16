ce68ea2070b85b78d7b4e8568522f2b916b10100
Eric Wallet <eric.wallet@yahoo.fr>
2017-05-16 08:24:10 +0200

    added first field to system

--- Resources/reactjs_app/src/components/data_types/string/StringField.jsx
+++ Resources/reactjs_app/src/components/data_types/string/StringField.jsx
@@ -7,8 +7,22 @@ export default class StringView extends React.Component {
     // 2 : name -> This is the object name (json id)
     // 3 ; data (full) -> The data object
 
+    constructor(props) {
+        super(props);
+        this.state = {value: this.props.data[this.props.name]};
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+
+    handleChange ( event ) {
+        //this.props.data[this.props.name]=value;
+        this.setState({value: event.target.value});
+    }
+
     render() {
-        return (<input type="text" className="form-control" id="form_control_1" placeholder="Enter your name" value={this.props.data[this.props.name]} />);
+        return (<input type="text" className="form-control" id="form_control_1" placeholder="Enter your name" value={this.state.value} onChange={this.handleChange} />);
     }
 
 }
