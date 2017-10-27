72a415f2fa9799f7f8bb0309478fb7e599faf65a
nicolas <nicrobelin@gmail.com>
2017-10-27 14:21:18 +0200

    delimiter bootstrap-tagsinput : #

--- Resources/public/assets/global/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js
+++ Resources/public/assets/global/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js
@@ -25,7 +25,7 @@
     maxTags: undefined,
     maxChars: undefined,
     confirmKeys: [13, 44],
-    delimiter: ',',
+    delimiter: '#',
     delimiterRegex: null,
     cancelConfirmKeysOnEmpty: false,
     onTagExists: function(item, $tag) {
